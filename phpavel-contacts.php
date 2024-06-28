1883a16757e67aea24b1a19f7e10686dc5d3f36d
philippovpavel-ru <74687078+philippovpavel-ru@users.noreply.github.com>
2024-06-28 13:04:33 +0300

    Update phpavel-contacts.php

--- phpavel-contacts.php
+++ phpavel-contacts.php
@@ -1,197 +1,277 @@
 <?php
-
 /**
- * Plugin Name: [ Контакты сайта ]
- * Description: Добавление раздела с контактами сайта
- *
- * Author URI:  https://philippovpavel.ru
- * Author:      Филиппов Павел
- *
- * Requires at least: 5.7
- * Requires PHP: 7.0
- *
- * License:     GPL2
- * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- *
- * Version:     1.0
- */
+Plugin Name: [ PHPavel Contact Page ]
+Description: Страница с полями контактов
+Author: PhilippovPavel
+Author URI: https://philippovpavel.ru/
+Version: 1.0.0
+Requires at least: 5.9
+Requires PHP: 8.1
 
+License:
+Released under the GPL license
+http://www.gnu.org/copyleft/gpl.html
+ **/
 
-if ( ! defined( 'ABSPATH' ) ) exit;
-// Page
-add_action( 'admin_menu', 'phpavel_contact_menu_page', 5 );
-function phpavel_contact_menu_page(){
-	add_submenu_page(
-		'options-general.php',
-		'Контакты',
-		'Контакты',
-		'manage_options',
-		'phpavel_contacts',
-		'phpavel_contact_page_callback'
-	);
-}
+if (!defined('ABSPATH')) exit;
 
-function phpavel_contact_page_callback(){
-	echo '<div class="wrap">
-		<h1>' . get_admin_page_title() . '</h1>
-		<form method="post" action="options.php">';
- 
-			settings_fields( 'phpavel_contacts_settings' );
-			do_settings_sections( 'phpavel_contacts' );
-			submit_button();
+register_uninstall_hook(__FILE__, ['phpavelContactPage', 'uninstall']);
 
-		echo '</form>
-	</div>';
-}
+class phpavelContactPage
+{
+	public static function uninstall()
+	{
+		if ( !current_user_can('activate_plugins') ) return;
 
-// Fields
-function fields() {
-	return array(
+		$fields = self::fields;
+		if ( !$fields ) return;
+
+		foreach ($fields as $field) {
+			$option_name = $field['id'];
+
+			delete_option($option_name);
+		}
+	}
+
+	private const fields = [
 		[
-			'id'       => 'phpavel_contact_phone',
+			'id'       => 'contact_phone',
 			'title'    => 'Телефон',
 			'type'     => 'tel',
 			'function' => 'sanitize_text_field'
 		],
 		[
-			'id'       => 'phpavel_contact_vk',
+			'id'       => 'contact_email',
+			'title'    => 'Email',
+			'type'     => 'email',
+			'function' => 'sanitize_text_field'
+		],
+		[
+			'id'       => 'contact_vk',
 			'title'    => 'Ссылка VK',
 			'type'     => 'url',
 			'function' => 'sanitize_text_field'
 		],
 		[
-			'id'       => 'phpavel_contact_inst',
-			'title'    => 'Ссылка Instagram',
-			'type'     => 'url',
-			'function' => 'sanitize_text_field'
-		],
-		[
-			'id'       => 'phpavel_contact_wa',
+			'id'       => 'contact_wa',
 			'title'    => 'Ссылка WhatsApp',
 			'type'     => 'url',
 			'function' => 'sanitize_text_field'
 		],
 		[
-			'id'       => 'phpavel_contact_address',
+			'id'       => 'contact_tg',
+			'title'    => 'Ссылка Telegram',
+			'type'     => 'url',
+			'function' => 'sanitize_text_field'
+		],
+		[
+			'id'       => 'contact_wb',
+			'title'    => 'Ссылка Wildberries',
+			'type'     => 'url',
+			'function' => 'sanitize_text_field'
+		],
+		[
+			'id'       => 'contact_address',
 			'title'    => 'Адрес',
 			'type'     => 'text',
 			'function' => 'sanitize_text_field'
 		],
 		[
-			'id'       => 'phpavel_contact_timeline',
-			'title'    => 'График работы',
+			'id'       => 'contact_map_xy',
+			'title'    => 'Карта, координаты',
 			'type'     => 'text',
 			'function' => 'sanitize_text_field'
 		],
 		[
-			'id'       => 'phpavel_contact_map',
-			'title'    => 'Карта координаты',
-			'type'     => 'text',
+			'id'       => 'contact_timeline',
+			'title'    => 'Время работы, описание',
+			'type'     => 'textarea',
+			'function' => 'sanitize_textarea_field'
+		],
+		[
+			'id'       => 'contact_cf7',
+			'title'    => 'Контактая форма',
+			'type'     => 'select_cf7',
 			'function' => 'sanitize_text_field'
 		],
+	];
 
-	);
-}
+	public function __construct()
+	{
+		add_action('admin_menu', [$this, 'contact_menu_page'], 5);
+		add_action('admin_init', [$this, 'contacts_fields']);
+		add_action('admin_enqueue_scripts', [$this, 'scripts_contact_page']);
+	}
 
-add_action( 'admin_init', 'phpavel_contacts_fields' );
-function phpavel_contacts_fields(){
-	$fields = fields();
-
-	add_settings_section( 'phpavel_contacts_settings_section_id', '', '', 'phpavel_contacts' );
-
-	foreach ($fields as $field) {
-		register_setting(
-			'phpavel_contacts_settings',
-			$field['id'],
-			$field['function']
+	function contact_menu_page()
+	{
+		add_submenu_page(
+			'options-general.php',
+			'Контакты',
+			'Контакты',
+			'manage_options',
+			'contacts',
+			[$this, 'contact_page_callback']
 		);
+	}
 
-		add_settings_field(
-			$field['id'],
-			$field['title'],
-			'phpavel_'. $field['type'] .'_field',
-			'phpavel_contacts',
-			'phpavel_contacts_settings_section_id',
-			array(
-				'label_for' => $field['id'],
-				'class'     => $field['id'] .'-item',
-				'name'      => $field['id'],
-			)
+	function contact_page_callback()
+	{
+		echo '<div class="wrap">
+			<h1>' . get_admin_page_title() . '</h1>
+			<form method="post" action="options.php">';
+
+		settings_fields('contacts_settings');
+		do_settings_sections('contacts');
+		submit_button();
+
+		echo '</form>
+		</div>';
+	}
+
+	function contacts_fields()
+	{
+		$fields = self::fields;
+
+		add_settings_section('contacts_settings_section_id', '', '', 'contacts');
+
+		foreach ($fields as $field) {
+			register_setting(
+				'contacts_settings',
+				$field['id'],
+				$field['function']
+			);
+
+			add_settings_field(
+				$field['id'],
+				$field['title'],
+				[$this, $field['type'] . '_field'],
+				'contacts',
+				'contacts_settings_section_id',
+				array(
+					'label_for' => $field['id'],
+					'class'     => $field['id'] . '-item',
+					'name'      => $field['id'],
+				)
+			);
+		}
+	}
+
+	// Sanitize functions
+	function text_field($args)
+	{
+		$value = get_option($args['name']);
+
+		printf(
+			'<input type="text" id="%s" name="%s" value="%s" class="regular-text" />',
+			esc_attr($args['name']),
+			esc_attr($args['name']),
+			esc_html($value)
 		);
+	}
+
+	function number_field($args)
+	{
+		$value = get_option($args['name']);
+
+		printf(
+			'<input type="number" min="1" id="%s" name="%s" value="%d" />',
+			esc_attr($args['name']),
+			esc_attr($args['name']),
+			absint($value)
+		);
+	}
+
+	function tel_field($args)
+	{
+		$value = get_option($args['name']);
+
+		printf(
+			'<input type="tel" id="%s" name="%s" value="%s" class="regular-text" />',
+			esc_attr($args['name']),
+			esc_attr($args['name']),
+			esc_html($value)
+		);
+	}
+
+	function email_field($args)
+	{
+		$value = get_option($args['name']);
+
+		printf(
+			'<input type="email" id="%s" name="%s" value="%s" class="regular-text" />',
+			esc_attr($args['name']),
+			esc_attr($args['name']),
+			esc_html($value)
+		);
+	}
+
+	function url_field($args)
+	{
+		$value = get_option($args['name']);
+
+		printf(
+			'<input type="url" id="%s" name="%s" value="%s" class="regular-text" />',
+			esc_attr($args['name']),
+			esc_attr($args['name']),
+			esc_url($value)
+		);
+	}
+
+	function textarea_field($args)
+	{
+		$value = get_option($args['name']);
+
+		printf(
+			'<textarea id="%s" name="%s" class="regular-text" rows="4">%s</textarea>',
+			esc_attr($args['name']),
+			esc_attr($args['name']),
+			esc_textarea($value)
+		);
+	}
+
+	function select_cf7_field($args)
+	{
+		$get_contact_forms = get_posts([
+			'post_type'   => 'wpcf7_contact_form',
+			'numberposts' => -1,
+			'post_status' => 'publish'
+		]);
+
+		if ( !class_exists('WPCF7') ) {
+			echo 'Плагин Contact Form 7 не установлен';
+			return;
+		}
+
+		$optionsArray = [];
+		$value = get_option($args['name']);
+
+		foreach ($get_contact_forms as $contact_form) {
+			$cfID = esc_attr($contact_form->ID);
+			$cfTitle = esc_html($contact_form->post_title);
+			$isSelected = selected($value, $cfID, false);
+
+			$optionsArray[] = "<option value='$cfID'$isSelected>$cfTitle</option>";
+		}
+
+		$optionsString = implode('', $optionsArray);
+
+		// echo '<pre>'. print_r( $get_contact_forms , 1) .'</pre>';
+
+		printf(
+			'<select id="%s" name="%s" class="regular-text">%s</select>',
+			esc_attr($args['name']),
+			esc_attr($args['name']),
+			$optionsString
+		);
+	}
+
+	function scripts_contact_page()
+	{
+		if ('settings_page_contacts' !== get_current_screen()->id) return;
+
+		wp_enqueue_script('phpavel-mask_phone', plugin_dir_url(__FILE__) . 'mask_phone.js', [], '1.0.0', true);
 	}
 }
 
-// Sanitize functions
-function phpavel_text_field( $args ){
-	$value = get_option( $args[ 'name' ] );
-
-	printf(
-		'<input type="text" id="%s" name="%s" value="%s" class="regular-text" />',
-		esc_attr( $args[ 'name' ] ),
-		esc_attr( $args[ 'name' ] ),
-		esc_html( $value )
-	);
-}
-
-function phpavel_number_field( $args ){
-	$value = get_option( $args[ 'name' ] );
-
-	printf(
-		'<input type="number" min="1" id="%s" name="%s" value="%d" />',
-		esc_attr( $args[ 'name' ] ),
-		esc_attr( $args[ 'name' ] ),
-		absint( $value )
-	);
-}
-
-function phpavel_tel_field( $args ){
-	$value = get_option( $args[ 'name' ] );
-
-	printf(
-		'<input type="tel" id="%s" name="%s" value="%s" class="regular-text" />',
-		esc_attr( $args[ 'name' ] ),
-		esc_attr( $args[ 'name' ] ),
-		esc_html( $value )
-	);
-}
-
-function phpavel_email_field( $args ){
-	$value = get_option( $args[ 'name' ] );
-
-	printf(
-		'<input type="email" id="%s" name="%s" value="%s" class="regular-text" />',
-		esc_attr( $args[ 'name' ] ),
-		esc_attr( $args[ 'name' ] ),
-		esc_html( $value )
-	);
-}
-
-function phpavel_url_field( $args ){
-	$value = get_option( $args[ 'name' ] );
-
-	printf(
-		'<input type="url" id="%s" name="%s" value="%s" class="regular-text" />',
-		esc_attr( $args[ 'name' ] ),
-		esc_attr( $args[ 'name' ] ),
-		esc_url( $value )
-	);
-}
-
-function phpavel_textarea_field( $args ){
-	$value = get_option( $args[ 'name' ] );
-
-	printf(
-		'<textarea id="%s" name="%s" class="regular-text" rows="4">%s</textarea>',
-		esc_attr( $args[ 'name' ] ),
-		esc_attr( $args[ 'name' ] ),
-		esc_textarea( $value )
-	);
-}
-
-// Scripts for Contact Page
-add_action( 'admin_enqueue_scripts', 'phpavel_scripts_contact_page' );
-function phpavel_scripts_contact_page() {
-	if ( 'settings_page_phpavel_contacts' !== get_current_screen()->id ) return;
-
-	wp_enqueue_script( 'phpavel-mask_phone', plugin_dir_url( __FILE__ ) .'mask_phone.js', [], plugin_dir_path( __FILE__ ) .'mask_phone.js', true );
-}
+$phpavelContactPage = new phpavelContactPage();
